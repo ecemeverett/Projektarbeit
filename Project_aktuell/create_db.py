52594ea0e392cac3ba4c0c07aeb3354381575381
ecemeverett <81993982+ecemeverett@users.noreply.github.com>
2024-10-21 00:02:15 +0200

    Update create_db.py

--- Project_aktuell/create_db.py
+++ Project_aktuell/create_db.py
@@ -8,7 +8,7 @@ def init_db():
         c.execute('''
             CREATE TABLE IF NOT EXISTS compliance (
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
-                url TEXT NOT NULL,
+                url TEXT NOT NULL UNIQUE,
                 conformity TEXT NOT NULL,
                 conformity_details BLOB NOT NULL
             )
