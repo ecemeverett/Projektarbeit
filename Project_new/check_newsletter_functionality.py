d16a123a7d2f506b950cdb5b3b09125aa16dec72
sunnymun <haseje01@thu.de>
2025-01-19 11:27:28 +0100

    Delete Project_new directory

--- Project_new/check_newsletter_functionality.py
+++
@@ -1,4 +0,0 @@
-
-#def check_newsletter_functionality(soup):
-    # Check the functinality of all 4 links and if they have the correct text
-    # Widerrufsrecht, Impressumslink, Datenschutzinformationen, Werbepartner
