398a85caa523ca5b6f46415cc0a3fd7e5d5822d9
ecemeverett <81993982+ecemeverett@users.noreply.github.com>
2025-01-28 00:01:26 +0100

    Update cookie_preference_clickable_links.py

--- Project_new/cookie_preference_clickable_links.py
+++ Project_new/cookie_preference_clickable_links.py
@@ -139,7 +139,7 @@ class CookiePreferenceLinkValidator:
     async def check_preference_links(self, url):
         """Checks for the presence of a cookie banner and validates Privacy Policy and Imprint links."""
         async with async_playwright() as p:
-            browser = await p.chromium.launch(headless=False)
+            browser = await p.chromium.launch(headless=True)
             context = await browser.new_context(
                 user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537"
             )
